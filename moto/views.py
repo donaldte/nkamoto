57e48b9aa08f1fb460e6c7de7516fb7533aea9e3
donaldte <donaldtedom0@gmail.com>
2023-09-11 00:35:41 +0100

    feat: refactor frontend

--- moto/views.py
+++ moto/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+
+class IndexView(View):
+    """
+    Name: IndexView
+    Description: This class is used to render the index page.
+    Author: DonaldProgrammeur 
+    """
+    def get(self, request):
+        return render(request, 'index.html')
